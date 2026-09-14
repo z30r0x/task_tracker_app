67fdbc3be25665032e2bf43b8c2cb431a632f41e
Toka Sayed <118456216+z30r0x@users.noreply.github.com>
2026-09-14 22:35:00 +0300

    mobile_framing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'features/home_page/home.dart';
@@ -60,10 +61,64 @@ class _TaskTrackerAppState extends State<TaskTrackerApp> {
           elevation: 0,
         ),
       ),
-      home: HomePage(
-        selectedColor: _accentColor,
-        onColorChanged: _changeAccentColor,
+      home: _WebPhoneFrame(
+        child: HomePage(
+          selectedColor: _accentColor,
+          onColorChanged: _changeAccentColor,
+        ),
       ),
+    );
+  }
+}
+
+class _WebPhoneFrame extends StatelessWidget {
+  const _WebPhoneFrame({required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    if (!kIsWeb) {
+      return child;
+    }
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        const phoneWidth = 375.0;
+        const phoneHeight = 812.0;
+        const outerPadding = 24.0;
+
+        final width = (constraints.maxWidth - outerPadding * 2)
+            .clamp(280.0, phoneWidth)
+            .toDouble();
+        final height = (constraints.maxHeight - outerPadding * 2)
+            .clamp(560.0, phoneHeight)
+            .toDouble();
+
+        return ColoredBox(
+          color: const Color(0xFFE8ECF1),
+          child: Center(
+            child: Container(
+              width: width,
+              height: height,
+              clipBehavior: Clip.antiAlias,
+              decoration: BoxDecoration(
+                color: const Color(0xFF080B10),
+                borderRadius: BorderRadius.circular(32),
+                border: Border.all(color: const Color(0xFF10161D), width: 8),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Color(0x40000000),
+                    blurRadius: 28,
+                    offset: Offset(0, 12),
+                  ),
+                ],
+              ),
+              child: child,
+            ),
+          ),
+        );
+      },
     );
   }
 }
